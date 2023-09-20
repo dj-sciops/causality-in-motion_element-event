b88612063ec0da72cf785de2b1a506e7e21a500a
Thinh Nguyen <thinh@vathes.com>
2023-09-20 13:27:40 -0500

    max varchar to 16250

--- element_event/trial.py
+++ element_event/trial.py
@@ -158,7 +158,7 @@ class Trial(dj.Imported):
         Attributes:
             Trial (foreign key): Trial table primary key.
             attribute_name ( varchar(32) ): Name of trial attribute
-            attribute_value ( varchar(16383) ): Optional. Trial attribute value
+            attribute_value ( varchar(16250) ): Optional. Trial attribute value
             attribute_blob (longblob): Optional. Trial attribute numerical data
         """
 
@@ -166,7 +166,7 @@ class Trial(dj.Imported):
         -> master
         attribute_name  : varchar(32)
         ---
-        attribute_value='': varchar(16383)
+        attribute_value='': varchar(16250)
         attribute_blob=null: longblob
         """
 
